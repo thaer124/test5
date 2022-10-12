663fff6f4c193521618fc3413408bca0c06273cb
thaer124 <thaer124_124@hotmail.com>
2022-10-12 21:32:21 +0200

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { StyledThemeProvider } from "@definitions/styled-components";
+import { Provider } from "react-redux";
+import store from "@redux/store";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <StyledThemeProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </StyledThemeProvider>
+  );
+}
+
+export default MyApp;
